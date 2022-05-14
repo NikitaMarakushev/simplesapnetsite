5f711069ebffef637612f0d89864cd3d99ae60e1
NikitaMarakushev <nikitamarakushev@yandex.ru>
2022-05-15 01:32:57 +0300

    Added first view and controller

---
+++ simplesapnetsite/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace simplesapnetsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
