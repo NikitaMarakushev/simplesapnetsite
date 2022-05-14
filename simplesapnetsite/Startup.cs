93c36861a5cf9941c9455d12ff487a5ed88fefca
NikitaMarakushev <nikitamarakushev@yandex.ru>
2022-05-15 01:28:38 +0300

    Добавьте файлы проекта.

---
+++ simplesapnetsite/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace simplesapnetsite
+{
+    public class Startup
+    {
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
+                .AddSessionStateTempDataProvider();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseRouting();
+
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}]");
+            });
+        }
+    }
+}
